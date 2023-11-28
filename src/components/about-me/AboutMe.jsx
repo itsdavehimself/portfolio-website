00156adcaac8f8a13f6f881437580df5fd7dd012
David <23064280+itsdavehimself@users.noreply.github.com>
2023-11-28 11:39:28 -0600

    Create about me component

---
+++ src/components/about-me/AboutMe.jsx
@@ -0,0 +1,9 @@
+import styles from './AboutMe.module.css';
+
+export default function AboutMe() {
+  return (
+    <section className={styles.container}>
+      <div>Stuff Goes Here...</div>
+    </section>
+  );
+}
