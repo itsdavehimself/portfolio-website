61cacad85433ba98833e7d72250b92a5ef05aed3
David <23064280+itsdavehimself@users.noreply.github.com>
2023-11-29 22:00:46 -0600

    Remove about me section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import './App.css';
-import AboutMe from './components/about-me/AboutMe';
 import HeroSection from './components/hero-section/HeroSection';
 import { CustomCursorContextProvider } from './context/CustomCursorContext';
 import CustomCursor from './components/custom-cursor/CustomCursor';
@@ -10,7 +9,6 @@ function App() {
       <div className="App">
         <CustomCursor />
         <HeroSection />
-        <AboutMe />
       </div>
     </CustomCursorContextProvider>
   );
