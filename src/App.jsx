e3e116cb1ee09324dadef0378d6751d9aa530cf7
David <23064280+itsdavehimself@users.noreply.github.com>
2023-12-01 22:18:05 -0600

    Add section components to main App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import './App.css';
-import HeroSection from './components/hero-section/HeroSection';
 import { CustomCursorContextProvider } from './context/CustomCursorContext';
 import CustomCursor from './components/custom-cursor/CustomCursor';
+import HeroSection from './components/hero-section/HeroSection';
+import AboutMe from './components/about-me/AboutMe';
+import Navbar from './components/navbar/Navbar';
+import SideDots from './components/side-dots/SideDots';
+import ScrollDown from './components/scroll-down/ScrollDown';
 
 function App() {
   return (
     <CustomCursorContextProvider>
       <div className="App">
         <CustomCursor />
+        <Navbar />
+        <SideDots />
         <HeroSection />
+        <ScrollDown />
+        <AboutMe />
       </div>
     </CustomCursorContextProvider>
   );
