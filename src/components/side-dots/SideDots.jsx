805c81cb5031dbf6c5044411b69289716ebbcb4d
David <23064280+itsdavehimself@users.noreply.github.com>
2023-12-05 07:47:19 -0600

    Animate in progress bar

--- src/components/side-dots/SideDots.jsx
+++ src/components/side-dots/SideDots.jsx
@@ -104,7 +104,8 @@ export default function SideDots({ scrollPosition }) {
     <AnimatePresence>
       {isVisible && (
         <motion.aside
-          initial={{ opacity: 1 }}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
           transition={{ duration: 0.5, ease: 'easeInOut' }}
         >
