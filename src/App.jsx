2c2b970d02277a646f6a3df9ecda7b00a63dfb43
David <23064280+itsdavehimself@users.noreply.github.com>
2023-12-04 21:36:27 -0600

    Add scroll progression line

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import './App.css';
 import { CustomCursorContextProvider } from './context/CustomCursorContext';
+import { useScroll, useMotionValueEvent } from 'framer-motion';
+import { useState } from 'react';
 import CustomCursor from './components/custom-cursor/CustomCursor';
 import HeroSection from './container/hero-section/HeroSection';
 import AboutMe from './container/about-me/AboutMe';
@@ -9,11 +11,18 @@ import Skills from './container/skills/Skills';
 import ContactMe from './container/contact-me/ContactMe';
 
 function App() {
+  const { scrollY } = useScroll();
+  const [scrollPosition, setScrollPosition] = useState(null);
+
+  useMotionValueEvent(scrollY, 'change', (latest) => {
+    setScrollPosition(latest);
+  });
+
   return (
     <CustomCursorContextProvider>
       <div className="App">
         <CustomCursor />
-        <SideDots />
+        <SideDots scrollPosition={scrollPosition} />
         <HeroSection />
         <AboutMe />
         <Work />
--- src/components/side-dots/SideDots.jsx
+++ src/components/side-dots/SideDots.jsx
@@ -1,15 +1,49 @@
 import styles from './SideDots.module.css';
+import PropTypes from 'prop-types';
+import { useEffect, useState } from 'react';
+import { motion } from 'framer-motion';
 
-export default function SideDots() {
+export default function SideDots({ scrollPosition }) {
+  const [lineHeight, setLineHeight] = useState(0);
+
+  useEffect(() => {
+    if (scrollPosition < 1000) {
+      setLineHeight(0);
+    } else if (scrollPosition <= 1100) {
+      setLineHeight(Math.min(scrollPosition / 16, 1100 / 16));
+    } else if (scrollPosition <= 2000) {
+      setLineHeight(1100 / 16);
+    } else if (scrollPosition <= 3000) {
+      setLineHeight(Math.min(scrollPosition / 16, 2100 / 16));
+    } else if (scrollPosition <= 3900) {
+      setLineHeight(3100 / 16);
+    } else {
+      setLineHeight(4100 / 16);
+    }
+  }, [scrollPosition]);
+
   return (
     <aside>
       <div className={styles['side-dots']}>
-        <div className={styles['side-dot']}></div>
-        <div className={styles['side-dot']}></div>
-        <div className={styles['side-dot']}></div>
-        <div className={styles['side-dot']}></div>
-        <div className={styles['side-dot']}></div>
+        <motion.div
+          style={{
+            position: 'absolute',
+            height: lineHeight,
+            width: 2,
+            backgroundColor: '#efefef',
+            transition: 'height 1s ease-in-out',
+          }}
+        ></motion.div>
+        <div className={styles['side-dot-filled']}></div>
+        <div className={styles['side-dot-empty']}></div>
+        <div className={styles['side-dot-empty']}></div>
+        <div className={styles['side-dot-empty']}></div>
+        <div className={styles['side-dot-empty']}></div>
       </div>
     </aside>
   );
 }
+
+SideDots.propTypes = {
+  scrollPosition: PropTypes.number,
+};
