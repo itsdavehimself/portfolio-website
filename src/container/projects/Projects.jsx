ab310c61c1e5d5f667e4f652109bce9558f531eb
David <23064280+itsdavehimself@users.noreply.github.com>
2023-12-06 17:51:10 -0600

    Add and style projects content

--- src/container/projects/Projects.jsx
+++ src/container/projects/Projects.jsx
@@ -1,9 +1,50 @@
 import styles from './Projects.module.css';
+import { useContext } from 'react';
+import { CustomCursorContext } from '../../context/CustomCursorContext';
+import ProjectsSideAnimation from '../../components/projects-side-animation/ProjectsSideAnimation';
 
 export default function Projects() {
+  const { setType } = useContext(CustomCursorContext);
+
+  const handleHoverProject = () => {
+    setType('hover-project');
+  };
+
+  const handleHoverProjectLeave = () => {
+    setType('default');
+  };
+
   return (
-    <section className={styles.container}>
-      <div>Recent Projects</div>
+    <section className={styles['projects-container']}>
+      <aside className={styles['projects-aside']}>
+        <ProjectsSideAnimation />
+      </aside>
+      <div className={styles['projects-main']}>
+        <div className={styles['projects-titles']}>
+          <h3
+            onMouseEnter={handleHoverProject}
+            onMouseLeave={handleHoverProjectLeave}
+            className={styles['project-title']}
+          >
+            CHAINSEEKER
+          </h3>
+          <h3
+            onMouseEnter={handleHoverProject}
+            onMouseLeave={handleHoverProjectLeave}
+            className={styles['project-title']}
+          >
+            1337MARKET
+          </h3>
+          <h3
+            onMouseEnter={handleHoverProject}
+            onMouseLeave={handleHoverProjectLeave}
+            className={styles['project-title']}
+          >
+            BATTLESHIP
+          </h3>
+        </div>
+        <div className={styles['projects-description']}></div>
+      </div>
     </section>
   );
 }
