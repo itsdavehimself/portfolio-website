a6ee273fa56035808578d4fd1d3e93ea62aab2c5
David <23064280+itsdavehimself@users.noreply.github.com>
2023-12-07 12:32:32 -0600

    Add content and styles for contact page

--- src/container/contact-me/ContactMe.jsx
+++ src/container/contact-me/ContactMe.jsx
@@ -1,9 +1,75 @@
 import styles from './ContactMe.module.css';
+import ContactHeader from '../../components/vertical-headers/contact-header/ContactHeader';
+import GithubLogo from '../../components/icons/GithubLogo';
+import InstagramLogo from '../../components/icons/InstagramLogo';
+import LinkedInLogo from '../../components/icons/LinkedInLogo';
+import { useContext } from 'react';
+import { CustomCursorContext } from '../../context/CustomCursorContext';
 
 export default function ContactMe() {
+  const { setType } = useContext(CustomCursorContext);
+
+  const handleHoverEmail = () => {
+    setType('hover-social');
+  };
+
+  const handleHoverEmailLeave = () => {
+    setType('default');
+  };
+
+  const handleEmailButtonClick = () => {
+    const emailAddress = 'davidsmolen@gmail.com';
+
+    const mailtoLink = `mailto:${emailAddress}`;
+
+    window.location.href = mailtoLink;
+  };
+
   return (
-    <section className={styles.container}>
-      <div>Contact Me</div>
+    <section className={styles['contact-container']}>
+      <aside className={styles['contact-aside']}>
+        <ContactHeader />
+      </aside>
+      <div className={styles['contact-main']}>
+        <div className={styles['contact-title']}>
+          <h2>CONNECTION PATHWAYS</h2>
+        </div>
+        <div className={styles['contact-links']}>
+          <div className={styles.socials}>
+            <button
+              className={styles['social-btn']}
+              onMouseEnter={handleHoverEmail}
+              onMouseLeave={handleHoverEmailLeave}
+            >
+              <GithubLogo />
+            </button>
+            <button
+              className={styles['social-btn']}
+              onMouseEnter={handleHoverEmail}
+              onMouseLeave={handleHoverEmailLeave}
+            >
+              <LinkedInLogo />
+            </button>
+            <button
+              className={styles['social-btn']}
+              onMouseEnter={handleHoverEmail}
+              onMouseLeave={handleHoverEmailLeave}
+            >
+              <InstagramLogo />
+            </button>
+          </div>
+          <div className={styles.email}>
+            <button
+              className={styles['email-btn']}
+              onClick={handleEmailButtonClick}
+              onMouseEnter={handleHoverEmail}
+              onMouseLeave={handleHoverEmailLeave}
+            >
+              DIGITAL MAIL ME
+            </button>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
