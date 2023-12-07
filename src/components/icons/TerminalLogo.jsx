c112a031d704202f527bb6a605bbba499a35b4b8
David <23064280+itsdavehimself@users.noreply.github.com>
2023-12-06 21:29:30 -0600

    Create skill icon components

---
+++ src/components/icons/TerminalLogo.jsx
@@ -0,0 +1,19 @@
+export default function TerminalLogo() {
+  return (
+    <svg
+      width="4rem"
+      height="4rem"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M12 19H21M3 5L11 12L3 19"
+        stroke="#efefef"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
